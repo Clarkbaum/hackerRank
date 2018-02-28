572176e1b67d20ad6168b7a35da15ecfaa0eb4f1
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2018-02-28 14:19:14 -0600

    class practice

--- insertNodeHeadLinkedList.js
+++ insertNodeHeadLinkedList.js
@@ -31,7 +31,7 @@ function insert(head, data) {
   }
 }
 
-//////////not from hacker rank just more practice
+//////////not from hacker rank just more practice//////////////
 
 var List = function(data) {
   this.data = data;
@@ -52,7 +52,6 @@ List.prototype.remove = function() {
     current = current.next
   }
   current.next = null;
-  console.log(current)
 }
 
 List.prototype.print = function() {
@@ -66,10 +65,84 @@ List.prototype.print = function() {
   console.log(output)
 }
 
+List.prototype.find = function(val) {
+  var current = this;
+  var output = false;
+  if(val === current.data) output = true;
+  while(current.next !== null) {
+    
+    current = current.next;
+    console.log("current.data", current.data)
+    if(val === current.data)  output = true
+  }
+  
+  console.log(output)
+}
+
+class List2 {
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  }
+  
+  add(data){
+    var current = this;
+    while(current.next !== null) {
+      current = current.next;
+    }
+    current.next = new List(data);;
+  }
+  
+  remove(){
+    var current = this;
+    while(current.next.next !== null) {
+      current = current.next
+    }
+    current.next = null;
+  }
+  
+  print(){
+    var output = [];
+    var current = this;
+    output.push(current.data)
+    while(current.next !== null) {
+      current = current.next;
+      output.push(current.data)
+    }
+    console.log(output)
+  }
+  
+  find(val) {
+    var current = this;
+    var output = false;
+    if(val === current.data) output = true;
+    while(current.next !== null) {
+      
+      current = current.next;
+      console.log("current.data", current.data)
+      if(val === current.data)  output = true
+    }
+    
+    console.log(output)
+  }
+}
+
+var list2 = new List2('1')
+list2.add('2');
+list2.add('3');
+list2.add('4');
+list2.find('1');
+list2.print()
+list2.remove();
+list2.find('4');
+list2.print()
+
 var list = new List('1');
 list.add('2');
 list.add('3');
 list.add('4');
+list.find('1');
 list.print()
 list.remove();
+list.find('4');
 list.print()
